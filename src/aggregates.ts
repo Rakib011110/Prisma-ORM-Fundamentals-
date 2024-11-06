db880a8f1ef8249a39ff1a5b95c32551bfdb34de
Rakibul Islam <rakib011110@gmail.com>
2024-11-06 14:44:15 +0600

    aggregate added

---
+++ src/aggregates.ts
@@ -0,0 +1,43 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const aggregrates = async () => {
+  const avgAge = await prisma.user.aggregate({
+    _avg: {
+      age: true,
+    },
+  });
+
+  // sum of age
+
+  const sumAge = await prisma.user.aggregate({
+    _sum: {
+      age: true,
+    },
+  });
+
+  // count of age
+  const countAge = await prisma.user.aggregate({
+    _count: {
+      age: true,
+    },
+  });
+  // max of age
+  const maxAge = await prisma.user.aggregate({
+    _max: {
+      age: true,
+    },
+  });
+  // min of age
+  const minAge = await prisma.user.aggregate({
+    _min: {
+      age: true,
+    },
+  });
+
+  const coundData = await prisma.user.count();
+
+  console.log(countAge);
+};
+aggregrates();
